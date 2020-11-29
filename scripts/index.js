0307c414454b6eed7ab7563c74de523d8020dd30
Omair Muhi <omairmuhi@icloud.com>
2020-11-29 11:08:20 -0500

    Subject: Trim white-space from user responses Problem:NA Solution:NA Note:NA

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer'); // command-line UI
 const fs = require('fs'); // FileSystem
-var licenseAbbrev = {
+let licenseAbbrev = {
     'MIT': 'MIT',
     'GNU GPLv3': 'GNUGPLv3',
     'Mozilla Public 2.0': 'MP2.0',
@@ -22,7 +22,7 @@ inquirer
     },
     {
         type: 'input',
-        message: 'Enter installation instructions for your project:',
+        message: 'Enter comma-separated installation instructions for your project:',
         name: 'installation',
     },
     {
@@ -37,7 +37,7 @@ inquirer
     },
     {
         type: 'input',
-        message: 'Enter test instructions for your project:',
+        message: 'Enter comma-separated test instructions for your project:',
         name: 'testing',
     },
     {
@@ -59,44 +59,64 @@ inquirer
     ])
     .then((response) => {
         // Empty readMeStr string
-        var readMeStr = '';
+        let readMeStr = '';
         // Add project title
-        var title = ('# ' + response.title + '\n\n');
+        let title = ('# ' + response.title.trim() + '\n\n');
         readMeStr += title;
         // Add project description
-        var description = '## ' + 'Description\n' + `${response.description}` + '\n\n';
+        let description = '## ' + 'Description\n' + `${response.description.trim()}` + '\n\n';
         readMeStr += description;
+        // Add TOC
+        let toc = '## ' + 'Table of Contents\n';
+        toc += '* [Installation](#installation)\n';
+        toc += '* [Usage](#usage)\n';
+        toc += '* [Contributing](#contributing)\n';
+        toc += '* [Tests](#tests)\n';
+        toc += '* [License](#license)\n';
+        toc += '* [Questions](#questions)\n\n';
+        readMeStr += toc;
         // Add installation instructions
-        var installation = '## ' + 'Installation\n' + `${response.installation}` + '\n\n';
+        let installationStepsArray = response.installation.split(",");
+        let installationListMd = '';
+        // process comma-separated data
+        for (let i = 0; i < installationStepsArray.length; i++) {
+            installationListMd += `${i + 1}. ${installationStepsArray[i].trim()}\n`;
+        }
+        let installation = '## ' + 'Installation\n' + `${installationListMd}` + '\n\n';
         readMeStr += installation;
         // Add usage information
-        var usage = '## ' + 'Usage\n' + `${response.usage}` + '\n\n';
+        let usage = '## ' + 'Usage\n' + `${response.usage.trim()}` + '\n\n';
         readMeStr += usage;
         // Add contributing information
-        var contributing = '## ' + 'Contributing\n' + `${response.contributing}` + '\n\n';
+        let contributing = '## ' + 'Contributing\n' + `${response.contributing.trim()}` + '\n\n';
         readMeStr += contributing;
         // Add testing information
-        var testing = '## ' + 'Tests\n' + `${response.testing}` + '\n\n';
+        let testStepsArray = response.testing.split(",");
+        let testListMd = '';
+        // process comma-separated data
+        for (let i = 0; i < testStepsArray.length; i++) {
+            testListMd += `${i + 1}. ${testStepsArray[i].trim()}\n`;
+        }
+        let testing = '## ' + 'Tests\n' + `${testListMd}` + '\n\n';
         readMeStr += testing;
         // Add license information
-        var license = '## ' + 'License\n' + 'This application is covered under the ' + `${response.license}` + ' license \n\n';
+        let license = '## ' + 'License\n' + 'This application is covered under the ' + `${response.license}` + ' license.\n\n';
         // Add license section to end of README
         readMeStr += license;
-        var licenseShield = `![license](https://img.shields.io/badge/license-${licenseAbbrev[response.license]}-brightgreen)` + '\n';
+        let licenseShield = `![license](https://img.shields.io/badge/license-${licenseAbbrev[response.license]}-brightgreen)` + '\n';
         // Add badge to top of README
         licenseShield += readMeStr;
         // Update README
         readMeStr = licenseShield;
         // Add github information
-        var githubLink = `https://github.com/${response.github}`;
-        var githubLinkMd = `[${githubLink}](${githubLink})`;
-        var github = '## ' + 'Questions\n' + `${response.github}: ${githubLinkMd}` + '\n';
+        let githubLink = `https://github.com/${response.github.trim()}`;
+        let githubLinkMd = `[${githubLink}](${githubLink})`;
+        let github = '## ' + 'Questions\n' + `${response.github.trim()}: ${githubLinkMd}` + '<br>';
         readMeStr += github;
         // Add email
-        readMeStr += `For additional questions, send an e-mail to: <${response.email}>\n\n`;
-
+        readMeStr += `For additional questions, send an e-mail to: <${response.email.trim()}>\n\n`;
         // write everything to file
-        fs.appendFile('my-project-06.md', readMeStr, (err) => {
+        fs.appendFile('output.md', readMeStr, (err) => {
             if (err !== null)
                 console.log(err);
         });
